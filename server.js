8283e1b7c5162bdb1ff66092a7c575504db610b7
Abhinav Raj <iamabhinav118@gmail.com>
2022-11-18 10:49:42 +0530

    changed port to 80

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(path.join(__dirname, "./build")));
-app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "./build/index.html")));
+app.get("/*", (req, res) =>
+  res.sendFile(path.join(__dirname, "./build/index.html"))
+);
 
-const PORT = process.env.PORT || 3009;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
